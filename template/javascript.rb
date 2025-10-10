f354b327245ca8c61e80b8659a616a781b5b4b2c
Achmad Chun Chun <anonychun@gmail.com>
2025-10-11 04:48:28 +0700

    fix: update regex for YARN_VERSION in Dockerfile to match latest version format

--- template/javascript.rb
+++ template/javascript.rb
@@ -3,7 +3,7 @@ after_bundle do
   yarn_lock_path = File.join(destination_root, "yarn.lock")
 
   if pnpm_version.present? && File.exist?(yarn_lock_path)
-    gsub_file "Dockerfile", /ARG YARN_VERSION=\d+\.\d+\.\d+/, "ARG PNPM_VERSION=#{pnpm_version}"
+    gsub_file "Dockerfile", /ARG YARN_VERSION=(\d+\.\d+\.\d+|latest)/, "ARG PNPM_VERSION=#{pnpm_version}"
     gsub_file "Dockerfile", "YARN_VERSION", "PNPM_VERSION"
     gsub_file "Dockerfile", "yarn.lock", "pnpm-lock.yaml"
     gsub_file "Dockerfile", "yarn install --immutable", "pnpm install --frozen-lockfile"
