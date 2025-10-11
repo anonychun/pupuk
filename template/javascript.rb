10c71d911f3997ce22929b88bae0c86ea5c59871
Achmad Chun Chun <anonychun@gmail.com>
2025-10-12 02:18:29 +0700

    fix: do not set yarn config on importmap and bun

--- template/javascript.rb
+++ template/javascript.rb
@@ -1,3 +1,5 @@
-create_file ".yarnrc.yml", <<~YAML
-  nodeLinker: pnpm
-YAML
+unless ["importmap", "bun"].include?(options[:javascript])
+  create_file ".yarnrc.yml", <<~YAML
+    nodeLinker: pnpm
+  YAML
+end
