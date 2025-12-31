2f62b737232caf5b5afdb5ab3b4bee4b4c6667f3
Achmad Chun Chun <anonychun@gmail.com>
2025-12-31 16:53:30 +0700

    feat: add corepack installation and configuration for Yarn in JavaScript template

--- template/javascript.rb
+++ template/javascript.rb
@@ -1,4 +1,8 @@
 unless ["importmap", "bun"].include?(options[:javascript])
+  run "npm install -g corepack"
+  run "corepack enable"
+  run "corepack prepare yarn@stable --activate"
+
   create_file ".yarnrc.yml", <<~YAML
     nodeLinker: pnpm
   YAML
